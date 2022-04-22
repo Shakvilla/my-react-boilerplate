2ac6c58f7b9d45fd1ea10375ca49b2ef9dd5a120
Shakvilla <abdulshakuraclement@yahoo.com>
2022-04-22 14:06:46 +0000

    added cypress configuration

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,21 @@
 const postcssConfig = require('./postcss.config')
 
+const cracoAlias = require('craco-alias')
+
 module.exports = {
   style: {
     postOptions: postcssConfig,
-  }
+  },
+  plugins: [
+    {
+      plugin: cracoAlias,
+      options: {
+        source: 'tsconfig',
+
+        baseUrl: './', 
+
+        tsConfigPath: './tsconfig.paths.json'
+      }
+    }
+  ]
 }

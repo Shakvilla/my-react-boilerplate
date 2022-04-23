36b8bb7299aa963a113b52c01bf76f87c6c0fa90
Shakvilla <abdulshakuraclement@yahoo.com>
2022-04-23 18:24:40 +0000

    added request cancelation

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,14 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
+// import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
+
+import axios, {AxiosInstance, Cancel} from 'axios'
+
+import {
+  ApiRequestConfig, 
+  WithAbortFn, 
+  ApiExecutor, 
+  ApiExecutorArgs, 
+  ApiError
+} from './api.types'
 
 const axiosParams = {
 
@@ -16,23 +26,67 @@ const axiosInstance = axios.create(axiosParams)
 
 //Main api function
 
+export const didAbort = (error: unknown) : error is Cancel & {aborted: boolean} => axios.isCancel(error)
+
+const getCancelSource = () => axios.CancelToken.source()
+
+export const isApiError = (error: unknown) : error is ApiError => {
+
+  return axios.isAxiosError(error)
+
+}
+
+const withAbort = <T>(fn: WithAbortFn) => {
+  const executor : ApiExecutor<T> = async (...args: ApiExecutorArgs) => {
+
+    const originalConfig = args[args.length - 1 ] as ApiRequestConfig
+
+    const {abort, ...config} = originalConfig
+
+    if(typeof abort === 'function'){
+      const { cancel, token} = getCancelSource()
+      config.cancelToken = token
+      abort(cancel)
+    }
+
+    try{
+      if(args.length > 2) {
+        const [url, body] = args
+        return await fn<T>(url, body, config)
+      }else {
+        const [url] = args
+        return await fn<T>(url, config)
+      }
+    } catch (error) {
+      console.log('api error', error)
+
+      if(didAbort(error)){
+        error.aborted = true
+      }
+
+      throw error
+    }
+  }
+
+  return executor
+}
 const api = (axios: AxiosInstance) => { 
   return {
-    get: <T>(url: string, config: AxiosRequestConfig = {}) =>
-      axios.get<T>(url, config),
+    get: <T>(url: string, config: ApiRequestConfig = {}) =>
+      withAbort<T>(axios.get)(url, config),
 
-    delete: <T>(url: string, config: AxiosRequestConfig) => 
-    axios.delete<T>(url, config),
+    delete: <T>(url: string, config: ApiRequestConfig) =>
+      withAbort<T>(axios.delete)(url, config),
 
-    post: <T>(url: string, body: unknown, config: AxiosRequestConfig) => 
-      axios.post<T>(url, body, config), 
-    
-    patch: <T>(url: string, body: unknown, config: AxiosRequestConfig) => 
-      axios.patch<T>(url, body, config),
+    post: <T>(url: string, body: unknown, config: ApiRequestConfig) =>
+      withAbort<T>(axios.post)(url, body, config),
 
-    put: <T>(url: string, body: unknown, config: AxiosRequestConfig) => 
-        axios.put<T>(url, body, config)
-  };
+    patch: <T>(url: string, body: unknown, config: ApiRequestConfig) =>
+      withAbort<T>(axios.patch)(url, body, config),
+
+    put: <T>(url: string, body: unknown, config: ApiRequestConfig) =>
+      withAbort<T>(axios.put)(url, body, config),
+  }
 };
 
 export default api(axiosInstance);
